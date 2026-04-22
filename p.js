eae4cda7910506d78cef0db9095fcf440cd36de6
preethi <preethijennifer1108@gmail.com>
2026-04-22 12:13:12 +0530

    sk movie details

---
+++ p.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector('h1');
+
+heading.addEventListener('click', function() {
+    alert('Vanakkam!');
+});
+
+console.log("we are Tamil Nadu actors")
+console.log("sivakartikeyan's latest movie is parasakthi")
